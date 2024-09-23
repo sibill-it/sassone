aad85d467f396040430e3a3a6c47eaf8a284066e
Luca Corti <luca@sibill.com>
2024-09-23 13:18:15 +0200

    Move Accumulating handler to separate file

--- lib/sassone/handler.ex
+++ lib/sassone/handler.ex
@@ -58,15 +58,6 @@ defmodule Sassone.Handler do
       end
   """
 
-  defmodule Accumulating do
-    @moduledoc false
-
-    @behaviour Sassone.Handler
-
-    @impl Sassone.Handler
-    def handle_event(event, data, state), do: {:ok, [{event, data} | state]}
-  end
-
   alias Sassone.XML
 
   @type t :: module()
