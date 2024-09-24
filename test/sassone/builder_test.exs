5b4c31be3421b35d9fd354fb26119af418e47303
Luca Corti <luca@sibill.com>
2024-09-24 19:59:14 +0200

    Failing tests for attribute and direct element content parsing

--- test/sassone/builder_test.exs
+++ test/sassone/builder_test.exs
@@ -8,7 +8,7 @@ defmodule Sassone.BuilderTest do
     test "encode simple schema" do
       alice = %Person{gender: "female", name: "Alice", surname: "Cooper", bio: "A nice girl."}
 
-      assert ~s|<person gender="female"><name>Alice</name><surname>Cooper</surname></person>| =
+      assert ~s|<person gender="female">A nice girl.<name>Alice</name><surname>Cooper</surname></person>| =
                Builder.build(alice)
                |> Sassone.encode!()
     end
@@ -16,15 +16,18 @@ defmodule Sassone.BuilderTest do
 
   describe "parsing" do
     test "decode simple schema" do
-      assert {:ok, {Person, attrs}} =
+      assert {:ok, {struct, attrs}} =
                Sassone.parse_string(
                  ~s|<person gender="male"><name>Bob</name><surname>Price</surname>A friendly mate.</person>|,
                  Builder.handler(%Person{}),
                  nil
                )
 
-      assert %Person{name: "Bob", surname: "Price"} =
-               struct(Person, attrs)
+      assert Person == struct
+      assert attrs.gender == "male"
+      assert attrs.name == "Bob"
+      assert attrs.surname == "Price"
+      assert attrs.bio == "A friendly mate."
     end
   end
 end
